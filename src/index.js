0b076aeec22aadfaa2d316f74539e15cdcfbdf8c
ShuumaIwamoto <iwa06ki12da@gmail.com>
2023-01-05 00:58:00 +0900

    不要なfile削除。 サイトからimageを取得

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg"
+    alt="Atomic Habits"
+  />
 );
+const Title = () => (
+  <h2>
+    Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones
+  </h2>
+);
+const Author = () => {
+  return <h4>James Clear</h4>;
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
