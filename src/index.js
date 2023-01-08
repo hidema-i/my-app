715d6d9cd0ccb058153b08abca4fe82a7a51daa3
ShuumaIwamoto <iwa06ki12da@gmail.com>
2023-01-08 23:31:24 +0900

    props

--- src/index.js
+++ src/index.js
@@ -1,40 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+
+const firstBook = {
+  author: "James Clear",
+  title:
+    "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break BadOnes",
+  img: "./images/book-1.jpg",
+};
+const secondBook = {
+  author: "Colleen Hoover",
+  title: "It Ends with Us: A Novel (1)",
+  img: "https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL600_SR600,400_.jpg",
+};
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      ></Book>
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   );
 };
 
-const Book = () => {
+const Book = (props) => {
+  const { img, title, author } = props;
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   );
-};
-
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg"
-    alt="Atomic Habits"
-  />
-);
-const Title = () => (
-  <h2>
-    Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones
-  </h2>
-);
-const Author = () => {
-  return <h4>James Clear</h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
