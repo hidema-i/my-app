4a1d67b78fbd1bd4c5dbe0c5e04adbd15fc83c3b
ShuumaIwamoto <iwa06ki12da@gmail.com>
2023-01-10 00:06:56 +0900

    修正

--- src/index.js
+++ src/index.js
@@ -2,31 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const firstBook = {
-  author: "James Clear",
-  title:
-    "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break BadOnes",
-  img: "./images/book-1.jpg",
-};
-const secondBook = {
-  author: "Colleen Hoover",
-  title: "It Ends with Us: A Novel (1)",
-  img: "https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL600_SR600,400_.jpg",
-};
+const books = [
+  {
+    author: "James Clear",
+    title:
+      "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break BadOnes",
+    img: "./images/book-1.jpg",
+    id: 1,
+  },
+  {
+    author: "Colleen Hoover",
+    title: "It Ends with Us: A Novel (1)",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL600_SR600,400_.jpg",
+    id: 2,
+  },
+];
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      ></Book>
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
+      {books.map((book) => {
+        // const { img, title, author, id } = book;
+        return <Book {...book} key={book.id} />;
+      })}
     </section>
   );
 };
