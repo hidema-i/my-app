d5cf48bf98bdd4285cec4001870775e7bbc82c8f
ShuumaIwamoto <iwa06ki12da@gmail.com>
2023-01-11 19:07:49 +0900

    components分割

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from "./images/book-1.jpg";
+import img2 from "./images/book-2.jpg";
+import img3 from "./images/book-3.jpg";
+
+export const books = [
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "Colleen Hoover",
+    title: "Starts with Us",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Colleen Hoover",
+    title: "Verity",
+    img: img3,
+    id: 3,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,74 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
-const books = [
-  {
-    author: "James Clear",
-    title:
-      "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break BadOnes",
-    img: "./images/book-1.jpg",
-    id: 1,
-  },
-  {
-    author: "Colleen Hoover",
-    title: "It Ends with Us: A Novel (1)",
-    img: "https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL600_SR600,400_.jpg",
-    id: 2,
-  },
-];
+import { books } from "./books";
+import Book from "./Book";
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <EventExamples />
-      {books.map((book) => {
+      {books.map((book, index) => {
         // const { img, title, author, id } = book;
-        return <Book {...book} key={book.id} />;
+        return <Book {...book} key={book.id} number={index} />;
       })}
     </section>
-  );
-};
-
-const EventExamples = () => {
-  const handleFormInput = (e) => {
-    console.log(e.target);
-    console.log(e.target.name);
-    console.log(e.target.value);
-    console.log("handle form input");
-  };
-  const handleButtonClick = () => {
-    alert("handle form input");
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("form input");
-  };
-  return (
-    <section>
-      <form onSubmit={handleSubmit}>
-        <h2>Typical Form</h2>
-        <input
-          type="text"
-          name="example"
-          onChange={handleFormInput}
-          style={{ margin: "1rem 0" }}
-        />
-      </form>
-      <button onClick={handleButtonClick}>click me</button>
-    </section>
-  );
-};
-
-const Book = (props) => {
-  const { img, title, author } = props;
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
   );
 };
 
